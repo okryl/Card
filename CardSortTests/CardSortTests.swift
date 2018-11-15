5a1eebd204969567ec1dc1850157d05bfcd5d505
Omer Karayel <omer.karayel@foreks.com>
2018-11-15 20:35:17 +0300

    Initial Commit

---
+++ CardSortTests/CardSortTests.swift
@@ -0,0 +1,34 @@
+//
+//  CardSortTests.swift
+//  CardSortTests
+//
+//  Created by Omer Karayel on 15.11.2018.
+//  Copyright © 2018 Omer Karayel. All rights reserved.
+//
+
+import XCTest
+@testable import CardSort
+
+class CardSortTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
