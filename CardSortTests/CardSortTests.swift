cd4c95ff55c081703cb40533c11811b4624557c6
Omer Karayel <omer.karayel@foreks.com>
2018-11-16 16:40:29 +0300

    Test case fix

--- CardSortTests/CardSortTests.swift
+++ CardSortTests/CardSortTests.swift
@@ -139,7 +139,7 @@ class CardSortTests: XCTestCase {
         
         let expectedIndexes =  [0,1,2,3,4,5,6,7,8,9,10]
         if serialSortCardIndexes == expectedIndexes,
-            suitSortCardIndexes == [],
+            suitSortCardIndexes == expectedIndexes,
             smartSortIndexes == expectedIndexes{
             XCTAssert(true)
         } else {
